8818ad108edc046ea626b4c9b0032de1d9c9965a
Pedro Neto <netopedroo117@gmail.com>
2025-03-26 17:53:22 -0300

    texto centralizado

--- src/app/_components/hero-section/_ui/texts-section.tsx
+++ src/app/_components/hero-section/_ui/texts-section.tsx
@@ -1,17 +1,16 @@
-import { Button } from "@/components/ui";
-import { ArrowUpRight } from "lucide-react";
+// import { Button } from "@/components/ui";
+// import { ArrowUpRight } from "lucide-react";
 import { Title, SpanWeightColor } from "@/components";
 
 export function TextsSection() {
   return (
     <section className="w-full flex flex-col md:flex-row items-center md:items-start justify-between">
-      <article className="w-full flex flex-col items-center sm:items-start gap-2">
+      <article className="w-full flex flex-col items-center text-center gap-2">
         <Title typography="h1" color="gradient" font="poppins">
-          Excelência em construção civil <br /> Qualidade e competência do
-          início ao fim
+        Excelencia , qualidade e competência do início ao fim
         </Title>
       </article>
-      <div className="w-full md:w-auto hidden md:flex md:flex-col gap-4 justify-center md:justify-end">
+      {/* <div className="w-full md:w-auto hidden md:flex md:flex-col gap-4 justify-center md:justify-end">
         <Title
           typography="h6"
           color="secondary"
@@ -36,7 +35,7 @@ export function TextsSection() {
             </div>
           </Button>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
